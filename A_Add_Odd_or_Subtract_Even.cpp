45cbcea7c432267e502f33a4e8fee75927117c0a
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-06 01:13:42 +0600

    2010 Problem A

--- A_Add_Odd_or_Subtract_Even.cpp
+++ A_Add_Odd_or_Subtract_Even.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
     int test; cin>>test;
     
-    while (test--) {
+    while (test--) {   
         int a,b; cin>>a>>b;
         if (a==b) {
             cout<<0<<endl;
         }
         else if (a<b) {
             int d=b-a;
-            if(d%2== 1)
+            if(d%2==1)
                 cout<<1<<endl;
             else
                 cout<<2<<endl;
@@ -20,7 +20,7 @@ int main(){
             if(d%2==0)
                 cout<<1<<endl;
             else
-                cout<<2<<endl;
+                cout<<2<<endl; 
         }
     }
     return 0;
