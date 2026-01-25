a9c53ca7d25555ace22b3190089b22f4e6ad6bc7
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-25 15:54:06 +0600

    Div-2 Contest-723 Problem-A

---
+++ A_Mean_Inequality.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int test ; cin>>test;
+    while (test--)
+    {
+        int n ; cin>>n ; 
+        n = n*2;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        int i = 0;
+        int j =n-1;
+        int count= 0;
+        while (i<=j)
+        {
+            if(count%2==0){
+                cout<<arr[j]<<" ";
+                j--;
+            }
+            else{
+                cout<<arr[i]<<" ";
+                i++;
+            }
+            count++;
+        }
+        cout<<endl;
+    }
+      
+    return 0;
+}
+/* ekhane age ami input nechi ... tarpor atake sort kore nibo... tarpor age bejor ta mane suru ta print korbo tarpor jor hole last theke print korbo */
