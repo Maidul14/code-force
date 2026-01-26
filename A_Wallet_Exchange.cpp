2831091a5f375a59d50739b2ca4d5b64e9576d86
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-27 02:07:06 +0600

    Contest-HElLO 2024 Problem-A

---
+++ A_Wallet_Exchange.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://codeforces.com/problemset/problem/1919/A
+int main(){
+    int test ; cin>>test;
+    while (test--)
+    {
+        int a,b; cin>>a>>b;
+        int res = a+b;
+        if(res%2==0){
+        cout<<"Bob"<<endl;
+        }
+        else{
+            cout<<"Alice"<<endl;
+        }
+    }
+    return 0;
+}
+/* ata problem a mulotho jor bijor niye khahini ... jor hole akta asbe ar bijor asbe akta  help form chat gtp */
