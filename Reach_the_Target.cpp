46e534ccffeeb4d69d98b805d3e23d8fab41955e
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-31 01:51:17 +0600

    Difficulty-281 Problem-REACHTARGET

---
+++ Reach_the_Target.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/REACHTARGET
+int main(){
+    int test ; cin>>test;
+    while (test--)
+    {
+        int a,b; cin>>a>>b;
+        cout<<a-b<<endl;
+    }
+      
+    return 0;
+}
