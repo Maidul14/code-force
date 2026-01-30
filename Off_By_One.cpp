fc8ae630f42d5a4aa64045b8e6647ee680e81a73
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-31 00:22:07 +0600

    Difficulty-271 Problem-OFFBY1

---
+++ Off_By_One.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/OFFBY1
+int main(){
+    int a,b; cin>>a>>b;
+    cout<<a+b<<1;  
+    return 0;
+}
