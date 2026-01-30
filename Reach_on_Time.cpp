a3a252cc08c29b1bd251d6a611d6218ef257c654
maidul14 <md.maidulislammahin14@gmail.com>
2026-01-31 01:06:12 +0600

    Difficulty-279 Problem-TIMELY

---
+++ Reach_on_Time.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+//problem link : https://www.codechef.com/problems/TIMELY
+int main(){
+    int test; cin>>test;
+    while (test--)
+    {
+        int n ; cin>>n;
+        if(n>=30){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+     
+    return 0;
+}
